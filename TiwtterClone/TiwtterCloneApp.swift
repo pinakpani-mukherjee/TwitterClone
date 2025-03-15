ad363dd49101809000b4658614c62a9ca8b278c5
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2025-03-15 16:37:07 +0900

    Initial Commit

---
+++ TiwtterClone/TiwtterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiwtterCloneApp.swift
+//  TiwtterClone
+//
+//  Created by Pinakpani Mukherjee on 2025/03/15.
+//
+
+import SwiftUI
+
+@main
+struct TiwtterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
