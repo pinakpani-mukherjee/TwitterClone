277f1ca46c9501b6924990eb54115ff52deb671c
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2025-04-24 16:18:58 +0900

    added the profile views

---
+++ TiwtterClone/View/Items/TabButton.swift
@@ -0,0 +1,18 @@
+//
+//  TabButton.swift
+//  TiwtterClone
+//
+//  Created by Pinakpani Mukherjee on 2025/04/24.
+//
+
+import SwiftUI
+
+struct TabButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabButton()
+}
---
+++ TiwtterClone/View/Profile/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  TiwtterClone
+//
+//  Created by Pinakpani Mukherjee on 2025/04/24.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}
